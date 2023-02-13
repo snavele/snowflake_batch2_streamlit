eedf15c684c8dd0611ffff0e7be6f180932bbe4d
snavele <125301485+snavele@users.noreply.github.com>
2023-02-14 02:10:27 +0800

    Update streamlit_app.py
    added emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Boiled Eggs')
-streamlit.text('Bread Omlet')
+streamlit.header('🥣 🥗 Breakfast Menu 🐔 🥑🍞')
+streamlit.text('🐔 Boiled Eggs')
+streamlit.text('🍞 Bread Omlet')
 streamlit.text('Tea/Coffee')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🍞 🐔 Hard-Boiled Free-Range Egg')
