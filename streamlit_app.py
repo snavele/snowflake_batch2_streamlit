8abf7af996a406b85178d2ae678884bed27562bb
snavele <125301485+snavele@users.noreply.github.com>
2023-02-14 02:12:10 +0800

    Update streamlit_app.py
    added dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('Tea/Coffee')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🍞 🐔 Hard-Boiled Free-Range Egg')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
