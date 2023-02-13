205e279425d8e3591e77383b594d826e7e9bdf3b
snavele <125301485+snavele@users.noreply.github.com>
2023-02-14 02:13:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🍞 🐔 Hard-Boiled Free-Range Egg')
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
