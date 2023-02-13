a55e7533fbddada133cdde1ac7f7566fb4b3d6ab
snavele <125301485+snavele@users.noreply.github.com>
2023-02-14 02:07:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Boiled Eggs')
+streamlit.text('Bread Omlet')
+streamlit.text('Tea/Coffee')
